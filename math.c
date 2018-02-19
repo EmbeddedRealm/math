78967035d8dc38002eaf3c737a1c0835e0023b22
Manuel Limones <limonesu.me@gmail.com>
2018-02-19 13:24:32 -0600

    Initial commit

---
+++ math.c
@@ -0,0 +1,15 @@
+int math_add(int a, int b){
+    return a + b;
+}
+
+int math_sub(int a, int b){
+    return a - b;
+}
+
+int math_mult(int a, int b){
+    return a * b;
+}
+
+int math_div(int a, int b){
+    return a / b;
+}
---
+++ math.h
@@ -0,0 +1,21 @@
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int math_add(int a, int b);
+
+int math_sub(int a, int b);
+
+int math_mul(int a, int b);
+
+int math_div(int a, int b);
+
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif
